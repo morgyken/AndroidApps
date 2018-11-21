87bc1b51c44edbe4923f2fe88d36fb5efea9971f
Morgyken <morgyken@gmail.com>
2018-11-21 10:58:17 +0300

    Added Threads Demo

--- MorThreadsApplications/app/src/main/java/org/clearleafs/morgyken/morthreadsapplications/MainActivity.java
+++ MorThreadsApplications/app/src/main/java/org/clearleafs/morgyken/morthreadsapplications/MainActivity.java
@@ -1,7 +1,9 @@
 package org.clearleafs.morgyken.morthreadsapplications;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void MorButtonClock (View view) {
+        long futuretime = System.currentTimeMillis() + 10000;
+        while(System.currentTimeMillis() < futuretime)
+        {
+            synchronized (this) {
+                try{
+                    wait(futuretime-System.currentTimeMillis());
+                }
+                catch (Exception e) {}
+            }
+        }
+        TextView morgyTextView = (TextView) findViewById(R.id.textView2);
+        morgyTextView.setText("Nice Work !");
     }
 }
